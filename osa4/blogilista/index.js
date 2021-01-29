ae941d8ae1dad0c27069249986fe7e6d75bfd13f
Jaakko <jaakko.vakeva@aalto.fi>
2021-01-29 16:30:41 +0200

    add ./controllers/notes.js for routers

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,40 +1,23 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const cors = require('cors')
 
+app.use(express.json())
 
 const Blog = require('./models/blog')
 
+const blogsRouter = require('./controllers/blogs')
+
+app.use('/api/blogs', blogsRouter)
 
 app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.get('/', (request, response) => {
-  logger.info("Hello world")
-})
 
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
